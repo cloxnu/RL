178a073b6bdc05f2675535362cd7faa4b4613ebc
CLOX <o18089541818@outlook.com>
2020-02-01 14:11:33 +0800

    1.1

--- maze/start.py
+++ maze/start.py
@@ -3,4 +3,23 @@ from QLearningTable import QLearningTable
 
 def update():
     for episode in range(100):
-        s = env.reset()
+        s = maze.reset()
+
+        while True:
+            maze.render()
+
+            action = RL.choose_action(str(s))
+            s_, reward, is_done = maze.step(action)
+            RL.train(str(s), action, reward, str(s_))
+            s = s_
+
+            if is_done:
+                break
+
+
+if __name__ == '__main__':
+    maze = Maze()
+    RL = QLearningTable(actions=list(range(maze.num_action)))
+
+    maze.after(100, update)
+    maze.mainloop()
